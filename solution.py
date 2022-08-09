cb0eaf0730844aefbc01b1c83470de21d309566b
Ali Akbari <7731.aa@gmail.com>
2022-08-09 09:50:11 +0430

    fix: calculator function

--- solution.py
+++ solution.py
@@ -1,5 +1,27 @@
-from itertools import count
-
-
 def calculator(n, m, li):
-    pass
+    newLi = []
+
+    counter = 0
+    sumOfItem = 0
+    while (counter < n):
+        if (m >= len(li)):
+            m = len(li)
+        for i in range(0, m):
+            sumOfItem += li[i]
+            counter += 1
+        newLi.append(sumOfItem)
+        sumOfItem = 0
+        del li[:m]
+    
+    result = 0
+    for i in range(0, len(newLi)):
+        if (i % 2 == 0):
+            number = newLi[i]
+        else:
+            number = -newLi[i]
+        result += number
+    
+    return result
+    
+
+print(calculator(8, 1, [1, 2, 3, 4, 5, 6, 7, 8]))
